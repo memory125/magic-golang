861fb02a1b60548d23f34a63b1ca38a04cbe5f3b
memory125 <mengde1120@sina.com>
2021-06-07 17:29:42 +0800

    update multiplicationtable.go

--- fundamental/multiplicationtable.go
+++ fundamental/multiplicationtable.go
@@ -21,13 +21,9 @@ func main()  {
 // 打印乘法表
 func multiplicationTable(num int)  {
 	for i := 1; i <= num; i++ {
-		for j := 1; i <= i; j++ {
-			fmt.Printf("%d * %d = %d", i, j, i * j)
-			fmt.Printf("  ")
-			if j == i {
-				fmt.Println()
-				break
-			}
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %d \t", i, j, i * j)
 		}
+		fmt.Println()
 	}
 }
