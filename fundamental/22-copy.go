8b2653f29f67f2cd789051d404d4ba8989aa40f8
memory125 <mengde1120@sina.com>
2021-06-10 17:28:53 +0800

    add copy.go

---
+++ fundamental/22-copy.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main()  {
+	sliceCopy()
+}
+
+func sliceCopy()  {
+	a1 := []int {1, 3, 5}
+	a2 := a1
+	a3 := make([]int, 3)
+	// 复制
+	copy(a3, a1)
+
+	/*
+		[1 3 5] [1 3 5] [1 3 5]
+	 */
+	fmt.Println(a1, a2, a3)
+
+	/*
+		[20 3 5] [20 3 5] [1 3 5]
+	 */
+	a1[0] = 20
+	fmt.Println(a1, a2, a3)
+
+	// 将a1中的索引为1的3这个元素删掉
+	/*
+		[20 5] [20 5 5] [1 3 5]
+	 */
+	a1 = append(a1[:1], a1[2:]...)
+	fmt.Println(a1, a2, a3)
+}
