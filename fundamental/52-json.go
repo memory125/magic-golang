300de4d0865b2db98ea507f8fb8a52d8e5513f6e
memory125 <mengde1120@sina.com>
2021-12-16 11:44:29 +0800

    add 52-json.go

---
+++ fundamental/52-json.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// 结构体与json
+
+// 1. 序列化：把Go语言中的结构体变量  ----> json格式的字符串
+// 2. 反序列化：json格式的字符串  ----> Go语言中能够识别的结构体变量
+
+type personInfo1 struct {
+	Name string `json:"name" db:"name" ini:"name"`
+	Age int     `json:"age" db:"age" ini:"age"`
+}
+
+func main()  {
+	p1 := personInfo1{
+		Name: "Gavin",
+		Age: 25,
+	}
+
+	// 序列化
+	b, err := json.Marshal(p1)
+	if err != nil {
+		fmt.Printf("p1 Marshal failed! Error is %v.\n", err)
+	}
+
+	/*
+		{"name":"Gavin","age":25}
+	 */
+	fmt.Println(string(b))
+
+	// 反序列化
+	str := `{"name": "Lee", "age": 36}`
+	var p2 personInfo1
+	json.Unmarshal([]byte(str), &p2)    // 传指针是为了在json.Unmarshal内部修改p2的值
+	/*
+		p2: main.personInfo1{Name:"Lee", Age:36}
+	 */
+	fmt.Printf("p2: %#v\n", p2)
+}
