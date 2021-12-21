3c49658b7b1801287ad5212082b08c92263877e8
memory125 <mengde1120@sina.com>
2021-12-21 11:30:23 +0800

    update calc.go

--- package/calcdemo.go
+++ package/calcdemo.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 )
 
+// 全局变量
+var x int = 20
+
+const pi = 3.1415926
+
+func init()  {
+	fmt.Printf("x = %v, pi = %v.\n", x, pi)
+}
+
 func main()  {
 	ret := calc.Add(50, 80)
 	/*
+		-------------------------Module cals---------------------
+		x = 20, pi = 3.1415926.
 		ret:  130
 	 */
 	fmt.Println("ret: ", ret)
