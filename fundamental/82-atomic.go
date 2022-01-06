64b67846adba939d2c303d68622b1c42636103e2
memory125 <mengde1120@sina.com>
2022-01-06 17:23:43 +0800

    update 82-atomic.go

--- fundamental/82-atomic.go
+++ fundamental/82-atomic.go
@@ -8,28 +8,28 @@ import (
 
 // 原子操作
 
-var x int64
-var wg6 sync.WaitGroup
+var x3 int64
+var wg7 sync.WaitGroup
 
 func addOp1() {
-	defer wg6.Done()
+	defer wg7.Done()
 	//x++
 	// 原子操作
 	/*
 		  类似于下述的操作：
 			lock.Lock()
-			x++
+			x3++
 			lock.Unlock()
 	*/
-	atomic.AddInt64(&x, 1)
+	atomic.AddInt64(&x3, 1)
 }
 
 func main() {
 	for i := 0; i < 1000000; i++ {
-		wg6.Add(1)
+		wg7.Add(1)
 		go addOp1()
 	}
-	wg6.Wait()
+	wg7.Wait()
 
-	fmt.Printf("Automic Operation ========== > finally x is %v.\n", x)
+	fmt.Printf("Automic Operation ========== > finally x3 is %v.\n", x3)
 }
