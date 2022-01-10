43d188ab6f5fb28771bcc26f4a194413d8de3d1e
memory125 <mengde1120@sina.com>
2022-01-10 16:16:31 +0800

    add http-client

---
+++ network/http/client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// http - client
+
+func main() {
+	resp, err := http.Get("http://127.0.0.1:6688/req")
+	if err != nil {
+		fmt.Printf("Http Get URL failed, error is %v.\n", err)
+		return
+	}
+
+	bytesRead, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Read response body failed, error is %v.\n", err)
+		return
+	}
+
+	fmt.Printf("Resp Body, %v.\n", string(bytesRead))
+}
--- network/http/server/server.go
+++ network/http/server/server.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 // http - server
 
@@ -10,7 +14,16 @@ func handle1(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte(str))
 }
 
+func handle2(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	fmt.Println(queryParams)
+	fmt.Println(r.Method)
+	fmt.Println(ioutil.ReadAll(r.Body))
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	http.HandleFunc("/wing", handle1)
-	http.ListenAndServe("127.0.0.1:6688", nil)
+	http.HandleFunc("/hello", handle1)
+	http.HandleFunc("/req", handle2)
+	_ = http.ListenAndServe("127.0.0.1:6688", nil)
 }
