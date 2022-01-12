5c6b523cf1c02451138a382591a35bb19cd35361
memory125 <mengde1120@sina.com>
2022-01-12 15:02:56 +0800

    update 85-flag.go

--- fundamental/85-flag.go
+++ fundamental/85-flag.go
@@ -24,4 +24,8 @@ func main() {
 		name is Jack.
 	*/
 	fmt.Printf("name is %v, age is %v \n", *name, *age)
+
+	fmt.Println("flag.Args() ", flag.Args())   // 返回命令行参数的其他参数，[]string类型
+	fmt.Println("flag.NArg() ", flag.NArg())   // 返回命令行参数后的其他参数个数
+	fmt.Println("flag.NFlag() ", flag.NFlag()) // 返回使用命令行参数个数
 }
