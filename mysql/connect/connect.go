a80fb826223cd68d628c32afca1f405fa8ce987f
memory125 <mengde1120@sina.com>
2022-01-13 13:49:00 +0800

    update connect.go

--- mysql/connect/connect.go
+++ mysql/connect/connect.go
@@ -3,7 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql" // init
+	_ "github.com/go-sql-driver/mysql" // init()
 )
 
 // MySQL - 连接示例
@@ -11,13 +11,13 @@ import (
 /*
 步骤：
    1. 下载mysql driver：go get -u github.com/go-sql-driver/mysql
-      dataSource格式：username:password@tcp(database server ip:3306)/go-test
+      dataSource格式：username:password@tcp(database server ip:port)/database name
    2. 创建go文件，调用database/sql包面的接口
 */
 
 func main() {
 	// 数据库连接信息
-	dataSource := "root:123456@tcp(192.168.40.133:3306)/go-test"
+	dataSource := "root:123456@tcp(192.168.40.133:3306)/go_test"
 	// 连接数据库
 	/*
 		备注：
